a0c85709cdd27fb9a4b2817b0a62294a7a8429b3
Patricio E. Arena <patricio.e.arena@gmail.com>
2021-10-12 19:50:09 -0300

    feat: httpClient se agregan metodos para verbos POST, PUT, DELETE

--- apiFrontend/src/main/java/com/example/apifrontend/controllers/ClientRestController.java
+++ apiFrontend/src/main/java/com/example/apifrontend/controllers/ClientRestController.java
@@ -1,33 +1,92 @@
 package com.example.apifrontend.controllers;
 
 import com.example.apifrontend.services.HttpClientAsynchronous;
+import com.example.apifrontend.services.NotAuthorizationHeaderException;
 import org.json.simple.JSONObject;
-import org.json.simple.parser.JSONParser;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestMethod;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.MediaType;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+import springfox.documentation.annotations.ApiIgnore;
 
-import java.util.List;
+import java.util.Map;
 
 @RestController
-@RequestMapping("api/ClientRest")
+@RequestMapping(name = "", value = "api/ClientRest", produces = MediaType.APPLICATION_JSON_VALUE)
 public class ClientRestController {
 
     private HttpClientAsynchronous httpClientAsynchronous;
 
-    private String urlTest1 = "https://pokeapi.co/api/v2/pokemon?limit=151";
-    private String urlTest2 = "https://pokeapi.co/api/v2/pokemon/1";
-
+    private String urlTestGet = "http://localhost:3000/getTest";
+    private String urlTestGetById = "http://localhost:3000/getTest/12";
+    private String urlTestPost = "http://localhost:3000/postTest";
+    private String urlTestPut = "http://localhost:3000/putTest/12";
+    private String urlTestDelete = "http://localhost:3000/deleteTest/12";
 
     @Autowired
     public ClientRestController(HttpClientAsynchronous httpClientAsynchronous) {
         this.httpClientAsynchronous = httpClientAsynchronous;
     }
 
-    @RequestMapping(value = "", produces = "application/json; charset=UTF-8", method = RequestMethod.GET)
-    public JSONObject Get() throws Exception {
-        return this.httpClientAsynchronous.Get( urlTest2);
+    @RequestMapping(value = "", method = RequestMethod.GET)
+    public ResponseEntity<JSONObject> Get(@ApiIgnore @RequestHeader Map<String, String> headers) throws Exception {
+        try {
+            JSONObject respose = this.httpClientAsynchronous.GET(headers, urlTestGet);
+            return new ResponseEntity<>(respose, HttpStatus.OK);
+        } catch (Exception e) {
+            if (e instanceof NotAuthorizationHeaderException)
+                return new ResponseEntity<>(HttpStatus.BAD_REQUEST);
+            return new ResponseEntity<>(HttpStatus.INTERNAL_SERVER_ERROR);
+        }
     }
 
+    @RequestMapping(value = "/{id}", method = RequestMethod.GET)
+    public ResponseEntity<JSONObject> GetById(@ApiIgnore @RequestHeader Map<String, String> headers) throws Exception {
+        try {
+            JSONObject respose = this.httpClientAsynchronous.GET(headers, urlTestGet);
+            return new ResponseEntity<>(respose, HttpStatus.OK);
+        } catch (Exception e) {
+            if (e instanceof NotAuthorizationHeaderException)
+                return new ResponseEntity<>(HttpStatus.BAD_REQUEST);
+            return new ResponseEntity<>(HttpStatus.INTERNAL_SERVER_ERROR);
+        }
+    }
+
+    @RequestMapping(value = "/post", method = RequestMethod.POST)
+    public ResponseEntity<JSONObject> Post(@ApiIgnore @RequestHeader Map<String, String> headers, @RequestBody Object data) throws Exception {
+        try {
+            JSONObject respose = this.httpClientAsynchronous.POST(headers,urlTestPost, data);
+            return new ResponseEntity<>(respose, HttpStatus.OK);
+        } catch (Exception e) {
+            if (e instanceof NotAuthorizationHeaderException)
+                return new ResponseEntity<>(HttpStatus.BAD_REQUEST);
+            return new ResponseEntity<>(HttpStatus.INTERNAL_SERVER_ERROR);
+        }
+    }
+
+    @RequestMapping(value = "/put", method = RequestMethod.PUT)
+    public ResponseEntity<JSONObject> Put(@ApiIgnore @RequestHeader Map<String, String> headers, @RequestBody Object data) throws Exception {
+        try {
+            JSONObject respose = this.httpClientAsynchronous.PUT(headers,urlTestPut, data);
+            return new ResponseEntity<>(respose, HttpStatus.OK);
+        } catch (Exception e) {
+            if (e instanceof NotAuthorizationHeaderException)
+                return new ResponseEntity<>(HttpStatus.BAD_REQUEST);
+            return new ResponseEntity<>(HttpStatus.INTERNAL_SERVER_ERROR);
+        }
+    }
+
+    @RequestMapping(value = "/delete", method = RequestMethod.DELETE)
+    public ResponseEntity<JSONObject> Delete(@ApiIgnore @RequestHeader Map<String, String> headers) throws Exception {
+        try {
+            JSONObject respose = this.httpClientAsynchronous.DELETE(headers, urlTestDelete);
+            return new ResponseEntity<>(respose, HttpStatus.OK);
+        } catch (Exception e) {
+            if (e instanceof NotAuthorizationHeaderException)
+                return new ResponseEntity<>(HttpStatus.BAD_REQUEST);
+            return new ResponseEntity<>(HttpStatus.INTERNAL_SERVER_ERROR);
+        }
+
+    }
 }
--- apiFrontend/src/main/java/com/example/apifrontend/services/HttpClientAsynchronous.java
+++ apiFrontend/src/main/java/com/example/apifrontend/services/HttpClientAsynchronous.java
@@ -1,6 +1,7 @@
 package com.example.apifrontend.services;
 
 
+import com.fasterxml.jackson.databind.ObjectMapper;
 import org.json.simple.JSONObject;
 import org.json.simple.parser.JSONParser;
 import org.json.simple.parser.ParseException;
@@ -10,39 +11,98 @@ import java.net.URI;
 import java.net.http.HttpClient;
 import java.net.http.HttpRequest;
 import java.net.http.HttpResponse;
+import java.util.Map;
 import java.util.concurrent.CompletableFuture;
+import java.util.concurrent.ExecutionException;
 import java.util.concurrent.TimeUnit;
 import java.time.Duration;
+import java.util.concurrent.TimeoutException;
 
 //https://programmerclick.com/article/92712109938/
 
 @Service
 public class HttpClientAsynchronous {
 
-    public HttpClientAsynchronous() {  }
+    public HttpClientAsynchronous() {
+    }
 
     private static final HttpClient httpClient = HttpClient.newBuilder()
             .version(HttpClient.Version.HTTP_2)
             .connectTimeout(Duration.ofSeconds(10))
             .build();
 
-    private JSONObject getJsonObject(String result) throws ParseException {
+    private static JSONObject getJsonObject(String result) throws ParseException {
         JSONParser parser = new JSONParser();
         return (JSONObject) parser.parse(result);
     }
 
-    public JSONObject Get(String uri) throws Exception {
-        HttpRequest request = HttpRequest.newBuilder()
-                .GET()
-                .uri(URI.create(uri))
-                .setHeader("User-Agent", "Java 11 HttpClient")
-                .build();
-
-        CompletableFuture<HttpResponse<String>> response = httpClient.sendAsync(request, HttpResponse.BodyHandlers.ofString());
-        String result = response.thenApply(HttpResponse::body).get(5, TimeUnit.SECONDS);
-        JSONObject json = getJsonObject(result);
-        return json;
+    private static void setHeaders(Map<String, String> headers, HttpRequest.Builder output) throws NotAuthorizationHeaderException {
+        if (!headers.containsKey("authorization"))
+            throw new NotAuthorizationHeaderException();
+        headers.forEach((key, value) -> {
+            if (key.equals("authorization")) {
+                output.header(key, value);
+            }
+            if (key.equals("content-type")) {
+                output.header(key, value);
+            }
+        });
     }
 
+    private JSONObject executeRequest(HttpRequest request) throws InterruptedException, ExecutionException, TimeoutException, ParseException {
+        CompletableFuture<HttpResponse<String>> response = httpClient.sendAsync(request, HttpResponse.BodyHandlers.ofString());
+        String result = response.thenApply(HttpResponse::body).get(5, TimeUnit.SECONDS);
+        return getJsonObject(result);
+    }
+
+    public JSONObject GET(Map<String, String> headers, String uri) throws Exception {
+        HttpRequest.Builder requestBuilder = HttpRequest.newBuilder();
+        setHeaders(headers, requestBuilder);
+        HttpRequest request = requestBuilder.GET().uri(URI.create(uri)).build();
+        return executeRequest(request);
+    }
+
+    public JSONObject POST(Map<String, String> headers, String uri, Object data) throws Exception {
+
+        ObjectMapper objectMapper = new ObjectMapper();
+        String requestBody = objectMapper
+                .writerWithDefaultPrettyPrinter()
+                .writeValueAsString(data);
+
+        HttpRequest.Builder requestBuilder = HttpRequest.newBuilder();
+        setHeaders(headers, requestBuilder);
+
+        HttpRequest request = requestBuilder
+                .POST(HttpRequest.BodyPublishers.ofString(requestBody))
+                .uri(URI.create(uri))
+                .build();
+
+        return executeRequest(request);
+    }
+
+    public JSONObject PUT(Map<String, String> headers, String uri, Object data) throws Exception {
+
+        ObjectMapper objectMapper = new ObjectMapper();
+        String requestBody = objectMapper
+                .writerWithDefaultPrettyPrinter()
+                .writeValueAsString(data);
+
+        HttpRequest.Builder requestBuilder = HttpRequest.newBuilder();
+        setHeaders(headers, requestBuilder);
+
+        HttpRequest request = requestBuilder
+                .PUT(HttpRequest.BodyPublishers.ofString(requestBody))
+                .uri(URI.create(uri))
+                .build();
+
+        return executeRequest(request);
+    }
+
+    public JSONObject DELETE(Map<String, String> headers, String uri) throws Exception {
+        HttpRequest.Builder requestBuilder = HttpRequest.newBuilder();
+        setHeaders(headers, requestBuilder);
+        HttpRequest request = requestBuilder.DELETE().uri(URI.create(uri)).build();
+        return executeRequest(request);
+    }
 
 }
