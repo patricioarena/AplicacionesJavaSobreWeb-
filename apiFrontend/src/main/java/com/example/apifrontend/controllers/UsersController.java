d528e87ad8dea3865d96e83ab09c39443f994a6a
Ayelen Palmieri <ayelenpalmieri9012@gmail.com>
2021-10-31 00:45:32 -0300

    vista usuarios admin

--- apiFrontend/src/main/java/com/example/apifrontend/controllers/UsersController.java
+++ apiFrontend/src/main/java/com/example/apifrontend/controllers/UsersController.java
@@ -2,14 +2,10 @@ package com.example.apiFrontend.controllers;
 
 import com.example.apiFrontend.models.User;
 import com.example.apiFrontend.services.HttpClientAsynchronous;
-import com.example.apiFrontend.services.NotAuthorizationHeaderException;
 import com.google.common.reflect.TypeToken;
 import com.google.gson.Gson;
 import org.json.simple.JSONObject;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.http.HttpStatus;
-import org.springframework.http.MediaType;
-import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.*;
@@ -44,6 +40,11 @@ public class UsersController {
             return new ResponseEntity<>(HttpStatus.INTERNAL_SERVER_ERROR);
         }
     }*/
+
+    @GetMapping("/base")
+    public String Test(){
+        return "base";
+    }
 
     @GetMapping("/getAll")
     public String userList(@RequestHeader Map<String, String> headers, Model model) throws Exception {
