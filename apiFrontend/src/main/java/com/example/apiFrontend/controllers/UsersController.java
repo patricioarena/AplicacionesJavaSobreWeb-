faff9520d0383b0e9e390da4520113d26a69cae5
Patricio Ernesto Antonio Arena <patricio.e.arena@gmail.com>
2021-11-29 18:10:47 -0300

    .

--- apiFrontend/src/main/java/com/example/apiFrontend/controllers/UsersController.java
+++ apiFrontend/src/main/java/com/example/apiFrontend/controllers/UsersController.java
@@ -12,6 +12,7 @@ import com.example.apiFrontend.services.*;
 import org.modelmapper.ModelMapper;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.*;
 import org.springframework.web.servlet.ModelAndView;
 import springfox.documentation.annotations.ApiIgnore;
