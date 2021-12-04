1dd1b31cdaa37879ff662cceb1bf7541436528f2
Patricio Ernesto Antonio Arena <patricio.e.arena@gmail.com>
2021-12-03 21:23:34 -0300

    branch para deploy

--- apiFrontend/src/main/java/com/example/apiFrontend/services/NotAuthorizationHeaderException.java
+++
@@ -1,7 +0,0 @@
-package com.example.apiFrontend.services;
-
-public class NotAuthorizationHeaderException extends Exception {
-    public NotAuthorizationHeaderException() {
-        super("No Authorization header found in request");
-    }
-}
